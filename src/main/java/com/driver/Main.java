9e46c8a53521289a349e892569439fa8c687276e
Manikandan <manikandan400800@gmail.com>
2023-06-09 12:02:56 +0530

    Method Over Ridind

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
+
 public class Main {
-  
+    public static class ClassA {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class ClassB extends ClassA{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args) {
+        ClassB b1 = new ClassB();
+        System.out.println("Before OverRiding : "+b1.meth());
+        ClassB b2 = new ClassB();
+        System.out.println("After OverRiding : "+b2.meth());
+    }
 }
